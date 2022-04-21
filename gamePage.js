5d7b83f825cdb3cefe9bea4cdc497480fad7cbfa
AnnikaSD <annika23@girlsms.org>
2022-04-20 17:52:59 -0700

    chaing stuff into comments as test

--- gamePage.js
+++ gamePage.js
@@ -7,26 +7,26 @@
  const getUserchoice = 0;
  console.log('inside js file');
  
-  if (getcomputerChoice === paper && getUserchoice === rock) { 
-   winner = 'The computer wins!';
-} else if  (getcomputerChoice === paper && getUserchoice === paper) { 
-    winner = "It's a Tie!";
- }  else if  (getcomputerChoice === paper && getUserchoice === scissors) { 
-    winner = 'You win!';
+//   if (getcomputerChoice === paper && getUserchoice === rock) { 
+//    winner = 'The computer wins!';
+// } else if  (getcomputerChoice === paper && getUserchoice === paper) { 
+//     winner = "It's a Tie!";
+//  }  else if  (getcomputerChoice === paper && getUserchoice === scissors) { 
+//     winner = 'You win!';
  
-} else if (getcomputerChoice === rock && getUserchoice === rock) { 
-    winner = "It's a Tie!";
- } else if  (getcomputerChoice === rock && getUserchoice === paper) { 
-     winner = "You win!";
-  }  else if  (getcomputerChoice === rock && getUserchoice === scissors) { 
-     winner = 'The computer wins!';
+// } else if (getcomputerChoice === rock && getUserchoice === rock) { 
+//     winner = "It's a Tie!";
+//  } else if  (getcomputerChoice === rock && getUserchoice === paper) { 
+//      winner = "You win!";
+//   }  else if  (getcomputerChoice === rock && getUserchoice === scissors) { 
+//      winner = 'The computer wins!';
   
-    } else if (getcomputerChoice === scissors && getUserchoice === rock) { 
-    winner = "YOu win!";
- } else if  (getcomputerChoice === scissors && getUserchoice === paper) { 
-     winner = "The computer wins! :D";
-  }  else if  (getcomputerChoice === scissors && getUserchoice === scissors) { 
-     winner = 'Uh oh, Its a tie!';
-  } 
+//     } else if (getcomputerChoice === scissors && getUserchoice === rock) { 
+//     winner = "YOu win!";
+//  } else if  (getcomputerChoice === scissors && getUserchoice === paper) { 
+//      winner = "The computer wins! :D";
+//   }  else if  (getcomputerChoice === scissors && getUserchoice === scissors) { 
+//      winner = 'Uh oh, Its a tie!';
+//   } 
   
   document.getElementById("winnertext").innerHTML = winner;
